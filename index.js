f88b5bab20761db5e6b95bcb748a6890641f1e73
Liz Burton <liz.burton147@gmail.com>
2017-10-26 19:06:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === whisper(string)) {
     return("I can\'t hear you!")
+  } else {
+    return("YES INDEED!")
   }
 }
+
+
